03057a2f5ec0e7bc842e53a26b7337f190a65295
Evan S <Gijera@Yahoo.com>
2019-04-27 16:49:04 -0600

    Initial VS 2017 Solution creation (Command Line .Net Core Project).

---
+++ price-calculator-kata_01/price-calculator-kata_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace price_calculator_kata_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
