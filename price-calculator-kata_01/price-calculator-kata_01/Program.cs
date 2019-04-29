e2fe02874568deae5c0b1f91e2b2168fcf38db28
Evan S <Gijera@Yahoo.com>
2019-04-29 16:23:20 -0600

    'Report' feature/requirement has already been implemented in prior commits.

--- price-calculator-kata_01/price-calculator-kata_01/Program.cs
+++ price-calculator-kata_01/price-calculator-kata_01/Program.cs
@@ -20,10 +20,14 @@ namespace price_calculator_kata_01
 
         private static void DisplayProduct(Product product)
         {
+            // Product & Price
             Console.WriteLine($"Product {product.Name}, was purchased for {product.Price.DecimalPlaces(2).CurrencyStr()}");
+            // Tax Report
             Console.WriteLine($"A {product.Tax.ToPercentage().PercentageStr()} tax, resulted in a tax of {product.CalculateTax().DecimalPlaces(2).CurrencyStr()}");
+            // Discount Report
             Console.WriteLine($"A {product.Discount.ToPercentage().PercentageStr()} discount, resulted in a discount of {product.CalculateDiscount().DecimalPlaces(2).CurrencyStr()}");
 
+            // Total
             Console.WriteLine($"For a total with tax and discount of: {product.CalculateTotalWithTaxAndDiscount().DecimalPlaces(2).CurrencyStr()}");
         }
     }
